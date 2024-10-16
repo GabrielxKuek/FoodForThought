69c8c0789bfb8d2a96f0cf65c5ac6486d0f9f975
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-10-16 11:22:38 +0800

    render please please please dont say im wrong

--- backend/main.py
+++ backend/main.py
@@ -9,4 +9,5 @@ app.register_blueprint(analyze_recipe_bp)
 app.register_blueprint(analyze_ingredients_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
